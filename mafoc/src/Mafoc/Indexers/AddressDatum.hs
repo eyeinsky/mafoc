b048de1305875ae7473f00ede79e73de5d458266
Markus Läll <markus.l2ll@gmail.com>
2023-10-02 17:26:36 +0300

    Inline implementation of `mafoc addressdatum`

--- mafoc/src/Mafoc/Indexers/AddressDatum.hs
+++ mafoc/src/Mafoc/Indexers/AddressDatum.hs
@@ -1,18 +1,20 @@
 {-# LANGUAGE NamedFieldPuns    #-}
+{-# LANGUAGE LambdaCase    #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLabels #-}
 module Mafoc.Indexers.AddressDatum where
 
-import Cardano.Api qualified as C
+import Options.Applicative qualified as Opt
 import Database.SQLite.Simple qualified as SQL
-import Marconi.ChainIndex.Indexers.AddressDatum qualified as Marconi
+
+import Cardano.Api qualified as C
 
 import Mafoc.CLI qualified as Opt
 import Mafoc.Core (DbPathAndTableName,
                    Indexer (Event, Runtime, State, checkpoint, description, initialize, parseCli, persistMany, toEvents),
-                   LocalChainsyncConfig_, blockChainPoint, defaultTableName, initializeLocalChainsync_,
-                   initializeSqlite, mkMaybeAddressFilter, setCheckpointSqlite, modifyStartingPoint)
-import Options.Applicative qualified as Opt
-
+                   LocalChainsyncConfig_, defaultTableName, initializeLocalChainsync_,
+                   initializeSqlite, mkMaybeAddressFilter, setCheckpointSqlite, modifyStartingPoint, txAddressDatums)
+import Mafoc.Indexers.Datum qualified as Datum
 
 data AddressDatum = AddressDatum
   { chainsync          :: LocalChainsyncConfig_
@@ -29,30 +31,66 @@ instance Indexer AddressDatum where
     <*> Opt.commonDbPathAndTableName
     <*> Opt.many Opt.commonAddress
 
-  newtype Event AddressDatum = Event (Marconi.StorableEvent Marconi.AddressDatumHandle)
+  data Event AddressDatum = Event
+    { slotNo :: C.SlotNo
+    , addressDatums :: [(C.AddressAny, C.Hash C.ScriptData)]
+    , datums :: [Event Datum.Datum]
+    }
 
   data State AddressDatum = EmptyState
 
   data Runtime AddressDatum = Runtime
     { sqlConnection      :: SQL.Connection
-    , tableName          :: String
+    , tablePrefix        :: String
+    , tableAddressDatums :: String
+    , tableDatums        :: String
     , maybeAddressFilter :: Maybe (C.Address C.ShelleyAddr -> Bool)
     }
 
-  toEvents Runtime{maybeAddressFilter} _state blockInMode@(C.BlockInMode (C.Block _ txs) _) = (EmptyState, [coerce event])
+  toEvents Runtime{maybeAddressFilter} _state blockInMode@(C.BlockInMode (C.Block _ txs) _) = (EmptyState, [event])
     where
-      event = Marconi.toAddressDatumIndexEvent maybeAddressFilter txs (blockChainPoint blockInMode)
-
-  persistMany Runtime{sqlConnection, tableName} events =
-    Marconi.sqliteInsert sqlConnection tableName $ coerce events
+      filter' = case maybeAddressFilter of
+        Just p -> filter ((\case C.AddressShelley addr -> p addr; _ -> False) . fst)
+        Nothing -> id
+      slotNo = #slotNo blockInMode
+      event = Event
+        { slotNo
+        , addressDatums = map (second (either id fst)) $ filter' $ txAddressDatums =<< txs
+        , datums = Datum.toEventsPrim (#slotNo blockInMode) txs
+        }
 
   initialize AddressDatum{chainsync, dbPathAndTableName, addresses} trace = do
     chainsyncRuntime <- initializeLocalChainsync_ chainsync trace
-    let (dbPath, tableName) = defaultTableName "address_datums" dbPathAndTableName
-    (sqlCon, checkpointedChainPoint) <- initializeSqlite dbPath tableName
-    Marconi.sqliteInit sqlCon tableName
-    let chainsyncRuntime' = modifyStartingPoint chainsyncRuntime (\cliChainPoint -> max checkpointedChainPoint cliChainPoint)
-    return (EmptyState, chainsyncRuntime', Runtime sqlCon tableName $ mkMaybeAddressFilter addresses)
+    let (dbPath, tablePrefix) = defaultTableName "address_datums" dbPathAndTableName
+        tableAddressDatums = tablePrefix <> "_address_datums"
+        tableDatums = tablePrefix <> "_datums"
+    (sqlCon, checkpointedChainPoint) <- initializeSqlite dbPath tablePrefix
+    sqliteInit sqlCon tableAddressDatums tableDatums
 
-  checkpoint Runtime{sqlConnection, tableName} _state slotNoBhh =
-    setCheckpointSqlite sqlConnection tableName slotNoBhh
+    let chainsyncRuntime' = modifyStartingPoint chainsyncRuntime (\cliChainPoint -> max checkpointedChainPoint cliChainPoint)
+    return (EmptyState, chainsyncRuntime', Runtime sqlCon tablePrefix tableAddressDatums tableDatums (mkMaybeAddressFilter addresses))
+
+  persistMany Runtime{sqlConnection, tableAddressDatums, tableDatums} events = do
+    sqliteInsertAddressDatums sqlConnection tableAddressDatums events
+    Datum.sqliteInsert sqlConnection tableDatums $ datums =<< events
+
+  checkpoint Runtime{sqlConnection, tablePrefix} _state slotNoBhh = setCheckpointSqlite sqlConnection tablePrefix slotNoBhh
+
+sqliteInit :: SQL.Connection -> String -> String -> IO ()
+sqliteInit sqlCon tableAddressDatums tableDatums = do
+  sqliteInitAddressDatums sqlCon tableAddressDatums
+  Datum.sqliteInit sqlCon tableDatums
+
+sqliteInitAddressDatums :: SQL.Connection -> String -> IO ()
+sqliteInitAddressDatums c tableAddressDatums = SQL.execute_ c $
+  " CREATE TABLE IF NOT EXISTS "
+  <> fromString tableAddressDatums
+  <> " ( address    TEXT NOT NULL \
+     \ , datum_hash BLOB NOT NULL \
+     \ , slot_no    INT  NOT NULL )"
+
+sqliteInsertAddressDatums :: SQL.Connection -> String -> [Event AddressDatum] -> IO ()
+sqliteInsertAddressDatums c tableName events = SQL.executeMany c template $ toSql =<< events
+  where
+    toSql Event{slotNo, addressDatums} = map (\(address, datumHash) -> (address, datumHash, slotNo)) addressDatums
+    template = "INSERT INTO " <> fromString tableName <> " VALUES (?, ?, ?, ?)" :: SQL.Query
