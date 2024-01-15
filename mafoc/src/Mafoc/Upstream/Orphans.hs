f04cbb756358abf5446bcd9328363e0ef014619c
Markus Läll <markus.l2ll@gmail.com>
2024-01-15 15:10:05 +0200

    Minor refactor orphans

--- mafoc/src/Mafoc/Upstream/Orphans.hs
+++ mafoc/src/Mafoc/Upstream/Orphans.hs
@@ -8,77 +8,137 @@ import Prettyprinter (Pretty (pretty), (<+>))
 import Data.Aeson qualified as Aeson
 import Data.Aeson (FromJSON, ToJSON)
 import Data.ByteString.Lazy (toStrict)
-
+import Database.SQLite.Simple qualified as SQL
+import Database.SQLite.Simple.Ok qualified as SQL
 import Codec.Serialise (Serialise (decode, encode))
 
-import Cardano.Binary (fromCBOR, toCBOR)
 import Cardano.Api qualified as C
 import Cardano.Api.Shelley qualified as C
-
 import Cardano.Binary qualified as CBOR
 import Cardano.Ledger.Shelley.API qualified as Ledger
 import Codec.CBOR.Read qualified as CBOR
 
-import Database.SQLite.Simple qualified as SQL
 
-import Database.SQLite.Simple.Ok qualified as SQL
+-- * C.Hash C.BlockHeader
 
-
--- | Hash BlockHeader
 deriving via C.UsingRawBytes (C.Hash C.BlockHeader) instance C.ToCBOR (C.Hash C.BlockHeader)
-
 deriving via C.UsingRawBytes (C.Hash C.BlockHeader) instance C.FromCBOR (C.Hash C.BlockHeader)
 
--- | C.TxIn
-deriving via C.UsingRawBytes C.TxId instance C.ToCBOR C.TxId
+instance Pretty (C.Hash C.BlockHeader) where
+  pretty hash = "BlockHash" <+> pretty (C.serialiseToRawBytesHexText hash)
 
+instance SQL.ToField (C.Hash C.BlockHeader) where
+  toField f = SQL.toField $ C.serialiseToRawBytes f
+
+instance SQL.FromField (C.Hash C.BlockHeader) where
+  fromField = fromFieldViaRawBytes (C.proxyToAsType Proxy)
+
+-- * C.TxIn
+
+instance C.ToCBOR C.TxIn where toCBOR (C.TxIn txId txIx) = CBOR.toCBOR (txId, txIx)
+instance C.FromCBOR C.TxIn where fromCBOR = uncurry C.TxIn <$> CBOR.fromCBOR
+
+instance SQL.ToRow C.TxIn where
+  toRow (C.TxIn txid txix) = SQL.toRow (txid, txix)
+
+instance SQL.FromRow C.TxIn where
+  fromRow = C.TxIn <$> SQL.field <*> SQL.field
+
+-- ** C.TxId
+
+deriving via C.UsingRawBytes C.TxId instance C.ToCBOR C.TxId
 deriving via C.UsingRawBytes C.TxId instance C.FromCBOR C.TxId
+
+instance SQL.FromField C.TxId where
+  fromField = fromFieldViaRawBytes C.AsTxId
+instance SQL.ToField C.TxId where
+  toField = SQL.SQLBlob . C.serialiseToRawBytes
+
+-- ** C.TxIx
+
 deriving newtype instance C.ToCBOR C.TxIx
 deriving newtype instance C.FromCBOR C.TxIx
-instance C.ToCBOR C.TxIn where toCBOR (C.TxIn txId txIx) = toCBOR (txId, txIx)
-instance C.FromCBOR C.TxIn where fromCBOR = uncurry C.TxIn <$> fromCBOR
 
--- | Value
-deriving newtype instance C.ToCBOR C.Quantity
+instance SQL.FromField C.TxIx where
+  fromField = fmap C.TxIx . SQL.fromField
+instance SQL.ToField C.TxIx where
+  toField (C.TxIx i) = SQL.SQLInteger $ fromIntegral i
 
-deriving newtype instance C.FromCBOR C.Quantity
-deriving newtype instance C.ToCBOR C.AssetName
-deriving newtype instance C.FromCBOR C.AssetName
-deriving via C.UsingRawBytes C.PolicyId instance C.ToCBOR C.PolicyId
-deriving via C.UsingRawBytes C.PolicyId instance C.FromCBOR C.PolicyId
+-- * C.Value
+
+instance C.ToCBOR C.Value where
+  toCBOR = CBOR.toCBOR . C.valueToList
+instance C.FromCBOR C.Value where
+  fromCBOR = C.valueFromList <$> CBOR.fromCBOR
+
+instance SQL.ToField C.Value where
+  toField = SQL.SQLBlob . toStrict . Aeson.encode
+
+instance SQL.FromField C.Value where
+  fromField f =
+    SQL.fromField f
+      >>= either
+        (const $ SQL.returnError SQL.ConversionFailed f "Cannot deserialise value.")
+        pure
+        . Aeson.eitherDecode
+
+-- ** C.AssetId
+
 instance C.ToCBOR C.AssetId where
   toCBOR =
-    toCBOR . \case
+    CBOR.toCBOR . \case
       C.AdaAssetId -> Nothing
       C.AssetId policyId assetName -> Just (policyId, assetName)
 instance C.FromCBOR C.AssetId where
-  fromCBOR = maybeToAssetId <$> fromCBOR
+  fromCBOR = maybeToAssetId <$> CBOR.fromCBOR
     where
       maybeToAssetId = \case
         Nothing -> C.AdaAssetId
         Just (policyId, assetName) -> C.AssetId policyId assetName
-instance C.ToCBOR C.Value where
-  toCBOR = toCBOR . C.valueToList
-instance C.FromCBOR C.Value where
-  fromCBOR = C.valueFromList <$> fromCBOR
 
--- | AddressAny
+-- ** C.Quantity
+
+deriving newtype instance C.ToCBOR C.Quantity
+deriving newtype instance C.FromCBOR C.Quantity
+deriving newtype instance SQL.ToField C.Quantity
+deriving newtype instance SQL.FromField C.Quantity
+
+deriving newtype instance SQL.ToField C.Lovelace
+deriving newtype instance SQL.FromField C.Lovelace
+
+-- ** C.AssetName
+
+deriving newtype instance C.ToCBOR C.AssetName
+deriving newtype instance C.FromCBOR C.AssetName
+deriving newtype instance SQL.ToField C.AssetName
+deriving newtype instance SQL.FromField C.AssetName
+
+-- ** C.PolicyId
+
+deriving via C.UsingRawBytes C.PolicyId instance C.ToCBOR C.PolicyId
+deriving via C.UsingRawBytes C.PolicyId instance C.FromCBOR C.PolicyId
+instance SQL.ToField C.PolicyId where -- C.PolicyId is a newtype over C.ScriptHash but no ToField available for it.
+  toField = SQL.toField . C.serialiseToRawBytes
+instance SQL.FromField C.PolicyId where
+  fromField = fromFieldViaRawBytes C.AsPolicyId
+
+-- * C.AddressAny
+
 deriving via C.UsingRawBytes C.AddressAny instance C.ToCBOR C.AddressAny
-
 deriving via C.UsingRawBytes C.AddressAny instance C.FromCBOR C.AddressAny
 
--- | Hash ScriptData
-deriving via C.UsingRawBytes (C.Hash C.ScriptData) instance C.ToCBOR (C.Hash C.ScriptData)
-
-deriving via C.UsingRawBytes (C.Hash C.ScriptData) instance C.FromCBOR (C.Hash C.ScriptData)
-
--- | SlotNo
+-- * C.SlotNo
 
 deriving newtype instance Real C.SlotNo
-
 deriving newtype instance Integral C.SlotNo
 
--- * ChainPoint
+instance Pretty C.SlotNo where
+  pretty (C.SlotNo n) = "Slot" <+> pretty n
+
+deriving newtype instance SQL.ToField C.SlotNo
+deriving newtype instance SQL.FromField C.SlotNo
+
+-- * C.ChainPoint
 
 instance Pretty C.ChainTip where
   pretty C.ChainTipAtGenesis = "ChainTipAtGenesis"
@@ -91,71 +151,18 @@ instance Pretty C.ChainPoint where
 instance SQL.FromRow C.ChainPoint where
   fromRow = C.ChainPoint <$> SQL.field <*> SQL.field
 
-instance SQL.ToRow C.ChainPoint where
-  toRow C.ChainPointAtGenesis = [SQL.SQLNull]
-  toRow (C.ChainPoint sn bh) = [SQL.toField sn, SQL.toField bh]
-
--- * C.Hash C.BlockHeader
-
-instance Pretty (C.Hash C.BlockHeader) where
-  pretty hash = "BlockHash" <+> pretty (C.serialiseToRawBytesHexText hash)
-
-instance SQL.ToField (C.Hash C.BlockHeader) where
-  toField f = SQL.toField $ C.serialiseToRawBytes f
-
-instance SQL.FromField (C.Hash C.BlockHeader) where
-  fromField f = do
-    bs <- SQL.fromField f
-    case C.deserialiseFromRawBytes (C.proxyToAsType Proxy) bs of
-      Left _ -> SQL.returnError SQL.ConversionFailed f "Cannot deserialise C.Hash C.BlockHeader"
-      Right x -> pure x
-
--- * Sometime we need to get a count or test if a value exist.
-
-instance SQL.ToRow Integer where
-  toRow = SQL.toRow
-
-instance SQL.FromRow Integer where
-  fromRow = SQL.field
-
--- * C.SlotNo
-
-instance Pretty C.SlotNo where
-  pretty (C.SlotNo n) = "Slot" <+> pretty n
-
-deriving newtype instance SQL.ToField C.SlotNo
-deriving newtype instance SQL.FromField C.SlotNo
-
-instance SQL.ToRow C.SlotNo where
-  toRow (C.SlotNo bn) = [SQL.toField bn]
-
-instance SQL.FromRow C.SlotNo where
-  fromRow = C.SlotNo <$> SQL.field
-
 -- * C.BlockNo
 
 instance Pretty C.BlockNo where
   pretty (C.BlockNo bn) = "BlockNo" <+> pretty bn
 
-instance SQL.FromRow C.BlockNo where
-  fromRow = C.BlockNo <$> SQL.field
-
-instance SQL.ToRow C.BlockNo where
-  toRow (C.BlockNo bn) = [SQL.toField bn]
+deriving newtype instance SQL.ToField C.BlockNo
+deriving newtype instance SQL.FromField C.BlockNo
 
 -- * C.AddressAny
 
 instance SQL.FromField C.AddressAny where
-  fromField f =
-    SQL.fromField f >>= \b ->
-      either
-        (const cantDeserialise)
-        pure
-        $ C.deserialiseFromRawBytes
-          C.AsAddressAny
-          b
-    where
-      cantDeserialise = SQL.returnError SQL.ConversionFailed f "Cannot deserialise address."
+  fromField = fromFieldViaRawBytes C.AsAddressAny
 
 instance SQL.ToField C.AddressAny where
   toField = SQL.SQLBlob . C.serialiseToRawBytes
@@ -174,27 +181,22 @@ instance ToJSON C.AddressAny where
 -- * C.Hash C.ScriptData
 
 instance SQL.FromField (C.Hash C.ScriptData) where
-  fromField f =
-    SQL.fromField f
-      >>= either
-        (const $ SQL.returnError SQL.ConversionFailed f "Cannot deserialise C.Hash C.ScriptData.")
-        pure
-        . C.deserialiseFromRawBytes (C.AsHash C.AsScriptData)
+  fromField = fromFieldViaRawBytes (C.AsHash C.AsScriptData)
 
 instance SQL.ToField (C.Hash C.ScriptData) where
   toField = SQL.SQLBlob . C.serialiseToRawBytes
 
 -- * C.ScriptData
 
+deriving via C.UsingRawBytes (C.Hash C.ScriptData) instance C.ToCBOR (C.Hash C.ScriptData)
+deriving via C.UsingRawBytes (C.Hash C.ScriptData) instance C.FromCBOR (C.Hash C.ScriptData)
+
 instance Serialise C.ScriptData where
   encode = CBOR.toCBOR
   decode = CBOR.fromCBOR
 
 instance SQL.FromField C.ScriptData where
-  fromField f =
-    SQL.fromField f
-      >>= either (const $ SQL.returnError SQL.ConversionFailed f "Cannot deserialise C.ScriptData.") pure
-        . C.deserialiseFromCBOR C.AsScriptData
+  fromField = fromFieldViaCbor' C.AsScriptData
 
 instance SQL.ToField C.ScriptData where
   toField = SQL.SQLBlob . C.serialiseToCBOR
@@ -210,44 +212,6 @@ mapLeft _ (Right v) = Right v
 
 instance ToJSON C.ScriptData where
   toJSON = C.scriptDataToJson C.ScriptDataJsonDetailedSchema . C.unsafeHashableScriptData
-
--- * C.TxIn
-
-instance SQL.ToRow C.TxIn where
-  toRow (C.TxIn txid txix) = SQL.toRow (txid, txix)
-
-instance SQL.FromRow C.TxIn where
-  fromRow = C.TxIn <$> SQL.field <*> SQL.field
-
-instance SQL.FromField C.TxId where
-  fromField f =
-    SQL.fromField f
-      >>= either
-        (const $ SQL.returnError SQL.ConversionFailed f "Cannot deserialise TxId.")
-        pure
-        . C.deserialiseFromRawBytes (C.proxyToAsType Proxy)
-
-instance SQL.ToField C.TxId where
-  toField = SQL.SQLBlob . C.serialiseToRawBytes
-
-instance SQL.FromField C.TxIx where
-  fromField = fmap C.TxIx . SQL.fromField
-
-instance SQL.ToField C.TxIx where
-  toField (C.TxIx i) = SQL.SQLInteger $ fromIntegral i
-
--- * C.Value
-
-instance SQL.ToField C.Value where
-  toField = SQL.SQLBlob . toStrict . Aeson.encode
-
-instance SQL.FromField C.Value where
-  fromField f =
-    SQL.fromField f
-      >>= either
-        (const $ SQL.returnError SQL.ConversionFailed f "Cannot deserialise value.")
-        pure
-        . Aeson.eitherDecode
 
 -- * C.ScriptInAnyLang
 
@@ -273,7 +237,7 @@ instance SQL.FromField C.ScriptHash where
       >>= either
         (const $ SQL.returnError SQL.ConversionFailed f "Cannot deserialise scriptDataHash.")
         pure
-        . C.deserialiseFromRawBytesHex (C.proxyToAsType Proxy)
+        . C.deserialiseFromRawBytesHex C.AsScriptHash
 
 -- * Ledger.Nonce
 
@@ -281,51 +245,34 @@ instance SQL.ToField Ledger.Nonce where
   toField = SQL.SQLBlob . CBOR.toStrictByteString . CBOR.toCBOR
 
 instance SQL.FromField Ledger.Nonce where
-  fromField f =
-    SQL.fromField f
-      >>= either
-        (const $ SQL.returnError SQL.ConversionFailed f "Cannot deserialise Ledger.Nonce.")
-        (pure . snd)
-        . CBOR.deserialiseFromBytes CBOR.fromCBOR
+  fromField = fromFieldViaCbor "Ledger.Nonce"
 
 -- * ToField/FromField
 
-deriving newtype instance SQL.ToField C.BlockNo
-deriving newtype instance SQL.FromField C.BlockNo
-
-deriving newtype instance SQL.ToField C.AssetName
-deriving newtype instance SQL.FromField C.AssetName
-
-deriving newtype instance SQL.ToField C.Quantity
-deriving newtype instance SQL.FromField C.Quantity
-
-instance SQL.ToField C.EpochNo where
-  toField (C.EpochNo word64) = SQL.toField word64
-instance SQL.FromField C.EpochNo where
-  fromField f = C.EpochNo <$> SQL.fromField f
-
-instance SQL.ToField C.Lovelace where
-  toField = SQL.toField @Integer . coerce
-instance SQL.FromField C.Lovelace where
-  fromField = coerce . SQL.fromField @Integer
+deriving newtype instance SQL.ToField C.EpochNo
+deriving newtype instance SQL.FromField C.EpochNo
 
 instance SQL.FromField C.PoolId where
-  fromField f = do
-    bs <- SQL.fromField f
-    case C.deserialiseFromRawBytes (C.AsHash C.AsStakePoolKey) bs of
-      Right h -> pure h
-      Left _ -> SQL.returnError SQL.ConversionFailed f " PoolId"
+  fromField = fromFieldViaRawBytes (C.AsHash C.AsStakePoolKey)
 
 instance SQL.ToField C.PoolId where
   toField = SQL.toField . C.serialiseToRawBytes
 
-instance SQL.ToField C.PolicyId where -- C.PolicyId is a newtype over C.ScriptHash but no ToField available for it.
-  toField = SQL.toField . C.serialiseToRawBytes
-instance SQL.FromField C.PolicyId where
-  fromField = fromFieldViaRawBytes C.AsPolicyId
+-- * Helpers
 
 -- | Helper to deserialize via SerialiseAsRawBytes instance
 fromFieldViaRawBytes :: (C.SerialiseAsRawBytes a) => C.AsType a -> SQL.Field -> SQL.Ok a
 fromFieldViaRawBytes as f = either (const err) pure . C.deserialiseFromRawBytes as =<< SQL.fromField f
   where
     err = SQL.returnError SQL.ConversionFailed f "can't deserialise via SerialiseAsRawBytes"
+
+-- | Helper to deserialize via SerialiseAsRawBytes instance
+fromFieldViaCbor :: CBOR.FromCBOR a => String -> SQL.Field -> SQL.Ok a
+fromFieldViaCbor str f = either (const err) (pure . snd) . CBOR.deserialiseFromBytes CBOR.fromCBOR =<< SQL.fromField f
+  where
+    err = SQL.returnError SQL.ConversionFailed f $ "can't deserialise " <> str <> " using FromCBOR"
+
+fromFieldViaCbor' :: C.SerialiseAsCBOR a => C.AsType a -> SQL.Field -> SQL.Ok a
+fromFieldViaCbor' as f = either (const err) pure . C.deserialiseFromCBOR as =<< SQL.fromField f
+  where
+    err = SQL.returnError SQL.ConversionFailed f $ "can't deserialise via CBOR"
